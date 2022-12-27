37b8c1f73b669b3cae1c7bf7a288790f5b3cd19e
chanhyungcho <jokuman7121@gmail.com>
2022-12-27 18:53:19 +0900

    nmovie-crawling

---
+++ DjangoProject/exrc/nlp/imdb/models.py
@@ -0,0 +1,68 @@
+import keras
+import matplotlib.pyplot as plt
+from keras import models, layers, utils, optimizers, callbacks
+
+
+class ImdbModel(object):
+    def __init__(self):
+        pass
+
+    def create(self, train_seq, val_seq):
+        global model,train_oh, val_oh
+        model = keras.Sequential()
+        sample_length = 100 #
+        freq_words = 500
+        model.add(keras.layers.SimpleRNN(8, input_shape=(sample_length, freq_words)))
+        model.add(keras.layers.Dense(1, activation='sigmoid'))
+        train_oh = keras.utils.to_categorical(train_seq)
+        print(train_oh.shape)
+        print(train_oh[0][0][:12])
+        val_oh= keras.utils.to_categorical(val_seq)
+
+
+    def fit(self,train_target,val_target):
+        rmsprop = keras.optimizers.RMSprop(learning_rate=1e-4)
+        model.compile(optimizer=rmsprop, loss='binary_crossentropy',metrics=['accuracy'])
+        checkpoint_cb = keras.callbacks.ModelCheckpoint('best-simplernn-model.h5',
+                                                         save_best_only=True)
+        early_stopping_cb = keras.callbacks.EarlyStopping(patience=3, restore_best_weights=True)
+        history = model.fit(train_oh, train_target, epochs=100, batch_size=64, validation_data=(val_oh, val_target),
+                            callbacks=[checkpoint_cb, early_stopping_cb])
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.xlabel('epoch')
+        plt.ylabel('loss')
+        plt.legend(['train','val'])
+        plt.show()
+
+
+class NaverMovieModel(object):
+    def __init__(self):
+        pass
+
+
+
+
+
+
+imdb_menu = ["Exit", #0
+                "hook"] #1
+imdb_lambda = {
+    "1" : lambda x: x.hook(),
+}
+if __name__ == '__main__':
+    imdb = ImdbModel()
+    while True:
+        [print(f"{i}. {j}") for i, j in enumerate(imdb_menu)]
+        menu = input('메뉴선택: ')
+        if menu == '0':
+            print("종료")
+            break
+        else:
+            try:
+                imdb_lambda[menu](imdb)
+            except KeyError as e:
+                if 'some error message' in str(e):
+                    print('Caught error message')
+                else:
+                    print("Didn't catch error message")
---
+++ DjangoProject/exrc/nlp/imdb/services.py
@@ -0,0 +1,93 @@
+import time
+from os import path
+
+import pandas as pd
+from bs4 import BeautifulSoup
+from keras.datasets import imdb
+from keras_preprocessing.sequence import pad_sequences
+from selenium import webdriver
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+from exrc.nlp.imdb.models import ImdbModel
+
+
+
+class ImdbService(object):
+    def __init__(self):
+        global train_input, train_target, test_input, test_target, train_input2, val_input, train_target2, val_target
+        (train_input, train_target), (test_input, test_target) = imdb.load_data(num_words=500)
+        train_input2, val_input, train_target2, val_target = train_test_split(train_input, train_target, test_size=0.2, random_state=42)
+
+    def hook(self):
+        model = ImdbModel()
+        # self.show_set()
+        # self.target_checker()
+        dc = self.txt_length()
+        model.create(dc['train_seq'],dc['val_seq'])
+        model.fit(train_target, val_target)
+
+    def show_set(self):
+        print(train_target[:20])
+        print(train_input.shape, test_input.shape)
+        print(len(train_input[0]))
+        print(train_input[0])
+
+    def target_checker(self):
+        print(train_target2[:20])
+        lengths = np.array([len(x) for x in train_input2])
+        print(f"평균값, 중간값 {np.mean(lengths)}, {np.median(lengths)}")
+        plt.hist(lengths)
+        plt.xlabel('lengths')
+        plt.ylabel('frequency')
+        plt.show()
+
+    def txt_length(self):
+        train_seq = pad_sequences(train_input, maxlen=100)
+        print(train_seq.shape)
+        print(train_seq[0])
+        print(train_input[0][:-10])
+        print(train_seq[5])
+        val_seq = pad_sequences(val_input, maxlen=100)
+        return {'train_seq': train_seq, 'val_seq': val_seq}
+
+
+
+class NaverMovieService(object):
+    def __init__(self):
+        global url, driver, file_name, encoding
+        url = 'https://movie.naver.com/movie/point/af/list.naver?&page='
+        driver = webdriver.Chrome(r'C:\Users\AIA\MsaProject\DjangoProject\exrc\webcrawler\chromedriver.exe')
+        file_name = r'C:\Users\AIA\MsaProject\DjangoProject\exrc\nlp\imdb\naver_movie_review_corpus.csv'
+        encoding = "UTF-8"
+
+    def crawling(self):
+        if not path.exists(file_name):
+            review_data = []
+            for page in range(1, 2):
+                driver.get(url + str(page))
+                soup = BeautifulSoup(driver.page_source, 'html.parser')
+                all_tds = soup.find_all('td', attrs={'class', 'title'})
+                for review in all_tds:
+                    need_reviews_cnt = 1000
+                    sentence = review.find("a", {"class": "report"}).get("onclick").split("', '")[2]
+                    if sentence != "":  # 리뷰 내용이 비어있다면 데이터를 사용하지 않음
+                        score = review.find("em").get_text()
+                        review_data.append([sentence, int(score)])
+            time.sleep(1)  # 다음 페이지를 조회하기 전 1초 시간 차를 두기
+            with open(file_name, 'w', newline='', encoding=encoding) as f:
+                wr = csv.writer(f)
+                wr.writerows(review_data)
+            driver.close()
+
+        data = pd.read_csv(file_name, header=None)
+        data.columns = ['review', 'score']
+        result = [print(f"{i + 1}. {data['score'][i]}\n{data['review'][i]}\n") for i in range(len(data))]
+        return result
+
+
+
+
+if __name__ == '__main__':
+    NaverMovieService().crawling()
